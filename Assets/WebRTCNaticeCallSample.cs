bad4e8829cc5e25e16ad5e6140a65a9f8730ced0
Makoto Hamanaka <ham.lua@gmail.com>
2018-02-26 02:47:09 +0900

    libjingle initialization success !

--- Assets/WebRTCNaticeCallSample.cs
+++ Assets/WebRTCNaticeCallSample.cs
@@ -5,22 +5,42 @@ using SimplePeerConnectionM;
 
 public class WebRTCNaticeCallSample : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    //bool first = true;
+
+    // Use this for initialization
+    void Start() {
+        Debug.Log("Sample.Start()");
+        InitWebRTC();
+    }
+
+    void InitWebRTC() {
 #if UNITY_ANDROID
+        AndroidJavaClass systemClass = new AndroidJavaClass("java.lang.System");
+        string libname = "jingle_peerconnection_so";
+        systemClass.CallStatic("loadLibrary", new object[1] { libname });
+        Debug.Log("loadLibrary loaded : "+ libname);
+
+        /*
+         * Below is equivalent of this java code:
+         * PeerConnectionFactory.InitializationOptions.Builder builder = 
+         *   PeerConnectionFactory.InitializationOptions.builder(UnityPlayer.currentActivity);
+         * PeerConnectionFactory.InitializationOptions options = 
+         *   builder.createInitializationOptions();
+         * PeerConnectionFactory.initialize(options);
+         */
 
         AndroidJavaClass playerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-
         AndroidJavaObject activity = playerClass.GetStatic<AndroidJavaObject>("currentActivity");
-
         AndroidJavaClass webrtcClass = new AndroidJavaClass("org.webrtc.PeerConnectionFactory");
-
+        AndroidJavaClass initOptionsClass = new AndroidJavaClass("org.webrtc.PeerConnectionFactory$InitializationOptions");
+        AndroidJavaObject builder = initOptionsClass.CallStatic<AndroidJavaObject>("builder", new object[1] { activity });
+        AndroidJavaObject options = builder.Call< AndroidJavaObject>("createInitializationOptions");
         if (webrtcClass != null)
 
         {
-
-            webrtcClass.CallStatic("initializeAndroidGlobals", new object[2] { activity, false });
-
+            Debug.Log("PeerConnectionFactory.initialize calling");
+            webrtcClass.CallStatic("initialize", new object[1] { options });
+            Debug.Log("PeerConnectionFactory.initialize called.");
         }
 
 #endif
@@ -31,6 +51,12 @@ public class WebRTCNaticeCallSample : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        /*
+		if (first)
+        {
+            first = false;
+            InitWebRTC();
+        }
+        */
 	}
 }
